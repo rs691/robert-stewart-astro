d0b57261b3100d1960343e4ed86c8629194b8743
rs691 <robstewart@my365.bellevue.edu>
2025-05-10 01:54:11 -0500

    added rest of files

---
+++ src/content.config.ts
@@ -0,0 +1,25 @@
+import { glob } from 'astro/loaders';
+import { defineCollection, z } from 'astro:content';
+
+
+
+export const collections = {
+	work: defineCollection({
+		// Load Markdown files in the src/content/work directory.
+		loader: glob({ base: './src/content/work', pattern: '**/*.md', }),
+		schema: z.object({
+			title: z.string(),
+			subtitle: z.string(),
+			description: z.string(),
+			publishDate: z.coerce.date(),
+			tags: z.array(z.string()),
+			email: z.string().optional(),
+			website: z.string().optional(),
+			github: z.string().optional(),
+			img: z.string().optional(),
+			img_alt: z.string().optional(),
+		}),
+	}),
+};
+
+
