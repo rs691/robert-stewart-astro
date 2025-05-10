74c55a8ca1fd7d07baac76c2c474ac5ef5033b8e
rs691 <robstewart@my365.bellevue.edu>
2025-05-09 21:47:32 -0500

    change yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://rs691.github.io',
+    base: '/robert-stewart-astro/',
+});
